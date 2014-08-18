0b37785bcd94285de2d0abcdb5383964d516e5c4
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2014-08-18 10:47:24 +0100

    Added main view

---
+++ Tweegle.Infrastructure.Configuration/ConfigurationReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tweegle.Infrastructure.Configuration
+{
+    public class ConfigurationReader : IConfigurationReader
+    {
+        public string GetScreenName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tweegle.Presentation.WebClient/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tweegle.Presentation.WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ImportAll()
+        {
+
+            return View();
+        }
+
+        public ActionResult ImportRecent()
+        {
+            return View();
+        }
+    }
+}
